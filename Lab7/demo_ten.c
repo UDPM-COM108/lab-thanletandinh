787ed11451b3374c524b38c7563a2b089a8e34cc
Than Le Tan Dinh <thanletandinh@gmail.com>
2025-11-27 16:20:38 +0700

    nộp demo lab 07

---
+++ Lab7/demo_ten.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    char ten[20];
+    int tuoi, diem;
+    printf("Nhập tên: ");
+    scanf("%s", ten);
+    fflush(stdin);
+    printf("Nhập tuổi: ");
+    scanf("%d", &tuoi);
+    printf("Nhập điểm: ");
+    scanf("%d", &diem);
+    getch();
+    printf("Họ và tên: %c\nTuổi: %d\nĐiểm: %d\n", ten, tuoi, diem);
+    return 0;
+}
